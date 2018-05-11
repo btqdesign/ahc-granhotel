ee37c68e0dbc214ee16636f60137a93819a3b28e
Saul <saul.diaz.v@gmail.com>
2018-05-11 18:25:13 -0500

    Fuente Butler

--- wp-content/themes/solaz-child/functions.php
+++ wp-content/themes/solaz-child/functions.php
@@ -3,6 +3,7 @@
 function solaz_child_scripts() {
     wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri(). '/style.css' );
     wp_enqueue_style( 'solaz-child-style', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style( 'butler', get_stylesheet_directory_uri() . '/assets/font-face/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'solaz_child_scripts' );
 
