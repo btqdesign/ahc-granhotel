4619ad338c582607a5255f3995c3fd0295831564
Saul <saul.diaz.v@gmail.com>
2018-05-11 19:42:10 -0500

    Dependencias

--- wp-content/themes/solaz-child/functions.php
+++ wp-content/themes/solaz-child/functions.php
@@ -2,8 +2,8 @@
 // push your child theme functions here
 function solaz_child_scripts() {
     wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri(). '/style.css' );
-    wp_enqueue_style( 'solaz-child-style', get_stylesheet_directory_uri() . '/style.css');
-    wp_enqueue_style( 'butler', get_stylesheet_directory_uri() . '/assets/font-face/font.css');
+    wp_enqueue_style( 'solaz-child-style', get_stylesheet_directory_uri() . '/style.css', 'solaz-parent-style');
+    wp_enqueue_style( 'butler', get_stylesheet_directory_uri() . '/assets/font-face/font.css', 'solaz-child-style');
 }
 add_action( 'wp_enqueue_scripts', 'solaz_child_scripts' );
 
