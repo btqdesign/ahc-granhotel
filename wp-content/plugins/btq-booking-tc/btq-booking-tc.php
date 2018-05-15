a3c3a750f362da7edb04bca677e70133bd62fa0f
Saul <saul.diaz.v@gmail.com>
2018-05-15 18:49:44 -0500

    Debug

--- wp-content/plugins/btq-booking-tc/btq-booking-tc.php
+++ wp-content/plugins/btq-booking-tc/btq-booking-tc.php
@@ -264,7 +264,7 @@ function btq_booking_tc_soap_query(){
 	$client = new nusoap_client($soap['wsaTo'], FALSE);
 	$client->decode_utf8 = TRUE;
 
-	$result = $client->call($soap['wsaTo'], $soap['envelope']);
+	$result = $client->call($soap['envelope'], $soap['wsaTo']);
 	
 	echo var_export($result, TRUE);
 }
