72819a8035235c1d9279a8bf7c4b25d79c6abcc4
Saul <saul.diaz.v@gmail.com>
2018-05-15 21:14:14 -0500

    Compatibilidad con soap

--- wp-content/themes/solaz-child/functions.php
+++ wp-content/themes/solaz-child/functions.php
@@ -10,5 +10,7 @@ add_action( 'wp_enqueue_scripts', 'solaz_child_scripts' );
 add_filter('final_output', function($output) {
     // Soporte HTTPS
     $output = str_replace('http:', 'https:', $output);
+    $output = str_replace('https://schemas.xmlsoap.org', 'http://schemas.xmlsoap.org', $output);
+    $output = str_replace('https://docs.oasisopen.org', 'http://docs.oasisopen.org', $output);
     return $output;
 });
