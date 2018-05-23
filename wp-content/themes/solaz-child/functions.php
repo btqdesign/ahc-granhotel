2251e0bbf7763921195dfa20a3a5584197723f06
Saul <saul.diaz.v@gmail.com>
2018-05-23 08:27:14 -0500

    que hay

--- wp-content/themes/solaz-child/functions.php
+++ wp-content/themes/solaz-child/functions.php
@@ -1,9 +1,9 @@
 <?php
 // push your child theme functions here
-solaz_parent_scripts() {
-	wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri(). '/style.css' );
+solaz_parent_btq_scripts() {
+	wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'solaz_parent_scripts');
+add_action( 'wp_enqueue_scripts', 'solaz_parent_btq_scripts' );
 
 function solaz_child_scripts() {
     wp_enqueue_style( 'solaz-child-style', get_stylesheet_directory_uri() . '/style.css', 'solaz-parent-style');
