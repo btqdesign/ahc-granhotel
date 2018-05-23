ae9b72490e6487e0016591223a876c0c6d5b75af
Saul <saul.diaz.v@gmail.com>
2018-05-23 08:24:08 -0500

    Carga de CSS en orden

--- wp-content/themes/solaz-child/functions.php
+++ wp-content/themes/solaz-child/functions.php
@@ -1,11 +1,15 @@
 <?php
 // push your child theme functions here
+solaz_parent_scripts() {
+	wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri(). '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'solaz_parent_scripts');
+
 function solaz_child_scripts() {
-    wp_enqueue_style( 'solaz-parent-style', get_template_directory_uri(). '/style.css' );
     wp_enqueue_style( 'solaz-child-style', get_stylesheet_directory_uri() . '/style.css', 'solaz-parent-style');
     wp_enqueue_style( 'butler', get_stylesheet_directory_uri() . '/assets/font-face/font.css', 'solaz-child-style');
 }
-add_action( 'wp_enqueue_scripts', 'solaz_child_scripts' );
+add_action( 'wp_enqueue_scripts', 'solaz_child_scripts', 1000);
 
 add_filter('final_output', function($output) {
     // Soporte HTTPS
