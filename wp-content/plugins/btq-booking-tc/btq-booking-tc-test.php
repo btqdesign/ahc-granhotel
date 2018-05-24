0eb01066cac1d771eab27c245cf9cfdf0a94b08e
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-05-24 11:21:47 -0500

    Mas filas

--- wp-content/plugins/btq-booking-tc/btq-booking-tc-test.php
+++ wp-content/plugins/btq-booking-tc/btq-booking-tc-test.php
@@ -184,6 +184,240 @@
 				<button class="buttonreserv">Reservar Ahora  </button>
 			</article>
 		</section>
+
+
+		<!-- OK -->
+		<section class="row">
+			
+			
+			<article class="col-md-5">
+				<div id="btq-carousel-00000" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#btq-carousel-00000" data-slide-to="0" class="active"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="1"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+					</ol>
+		
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla1.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla2.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla3.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla4.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+					</div>
+					
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#btq-carousel-00000" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+						<span class="sr-only">Anterior</span>
+					</a>
+					<a class="right carousel-control" href="#btq-carousel-00000" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+						<span class="sr-only">Siguiente</span>
+					</a>
+				</div>
+			</article>
+		
+			<article class="col-md-4">
+
+				<h3 class="titulo">Habitacion de Lujo King No Reembolsable</h3>
+
+				<aside>La habitación de Lujo King No Reembolsable cuenta con una cama King size y vista a la calle 16 de septiembre. Televisor LCD de 50 pulgadas con control remoto y canales de cable, aire acondicionado, 
+				<a href="#" id="vermas">Ver más</a>
+
+				<aside id="mostrar" style="display:none">mini bar, sala de bano privada con regadera y bañera, espejo de vanidad, amenidades, secadora de cabello, cafetera y kit de cafe, closet, caja de seguridad, plancha y mesa de planchado, escritorio de trabajo, conexion wi-fi y alambrica a internet, llave electronica y telefono con linea directa. PREPAGO: SE LE COBRARÁ UN PREPAGO DEL PRECIO TOTAL EN CUALQUIER MOMENTO, CANCELACIÓN: SE LE COBRARÁ EL PRECIO TOTAL SI CANCELA SU RESERVA O SI ES NO SHOW</aside></aside> 
+				
+				
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-04.png', __FILE__ ); ?>" alt="icono">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-03.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-01.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-05.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<hr class="linealetras" style="border-color:#C9B891;" style="border:2px;" />
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_like.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_c.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+			</article>
+		
+			<article class="col-md-3">
+				<br>
+				<input type="checkbox"> Mejor tarifa garantizada <p>$MXN 2,733.03</p>
+				<p>Con mas beneficios</p>
+				<hr class="linea"/>
+				<h3 align="center">$MXN 2,733.03   /noche</h3>
+				<button class="buttonreserv">Reservar Ahora  </button>
+			</article>
+		</section>
+
+
+		<!-- OK -->
+		<section class="row">
+			
+			
+			<article class="col-md-5">
+				<div id="btq-carousel-00000" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#btq-carousel-00000" data-slide-to="0" class="active"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="1"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+					</ol>
+		
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla1.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla2.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla3.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla4.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+					</div>
+					
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#btq-carousel-00000" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+						<span class="sr-only">Anterior</span>
+					</a>
+					<a class="right carousel-control" href="#btq-carousel-00000" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+						<span class="sr-only">Siguiente</span>
+					</a>
+				</div>
+			</article>
+		
+			<article class="col-md-4">
+
+				<h3 class="titulo">Habitacion de Lujo King No Reembolsable</h3>
+
+				<aside>La habitación de Lujo King No Reembolsable cuenta con una cama King size y vista a la calle 16 de septiembre. Televisor LCD de 50 pulgadas con control remoto y canales de cable, aire acondicionado, 
+				<a href="#" id="vermas">Ver más</a>
+
+				<aside id="mostrar" style="display:none">mini bar, sala de bano privada con regadera y bañera, espejo de vanidad, amenidades, secadora de cabello, cafetera y kit de cafe, closet, caja de seguridad, plancha y mesa de planchado, escritorio de trabajo, conexion wi-fi y alambrica a internet, llave electronica y telefono con linea directa. PREPAGO: SE LE COBRARÁ UN PREPAGO DEL PRECIO TOTAL EN CUALQUIER MOMENTO, CANCELACIÓN: SE LE COBRARÁ EL PRECIO TOTAL SI CANCELA SU RESERVA O SI ES NO SHOW</aside></aside> 
+				
+				
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-04.png', __FILE__ ); ?>" alt="icono">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-03.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-01.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-05.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<hr class="linealetras" style="border-color:#C9B891;" style="border:2px;" />
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_like.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_c.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+			</article>
+		
+			<article class="col-md-3">
+				<br>
+				<input type="checkbox"> Mejor tarifa garantizada <p>$MXN 2,733.03</p>
+				<p>Con mas beneficios</p>
+				<hr class="linea"/>
+				<h3 align="center">$MXN 2,733.03   /noche</h3>
+				<button class="buttonreserv">Reservar Ahora  </button>
+			</article>
+		</section>
+
+
+		<!-- OK -->
+		<section class="row">
+			
+			
+			<article class="col-md-5">
+				<div id="btq-carousel-00000" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#btq-carousel-00000" data-slide-to="0" class="active"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="1"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+						<li data-target="#btq-carousel-00000" data-slide-to="2"></li>
+					</ol>
+		
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla1.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla2.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla3.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+						<div class="item">
+							<img src="<?php echo plugins_url( $images_dir . DIRECTORY_SEPARATOR . 'sencilla4.png', __FILE__ ); ?>" alt="Habitaciones">
+						</div>
+					</div>
+					
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#btq-carousel-00000" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+						<span class="sr-only">Anterior</span>
+					</a>
+					<a class="right carousel-control" href="#btq-carousel-00000" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+						<span class="sr-only">Siguiente</span>
+					</a>
+				</div>
+			</article>
+		
+			<article class="col-md-4">
+
+				<h3 class="titulo">Habitacion de Lujo King No Reembolsable</h3>
+
+				<aside>La habitación de Lujo King No Reembolsable cuenta con una cama King size y vista a la calle 16 de septiembre. Televisor LCD de 50 pulgadas con control remoto y canales de cable, aire acondicionado, 
+				<a href="#" id="vermas">Ver más</a>
+
+				<aside id="mostrar" style="display:none">mini bar, sala de bano privada con regadera y bañera, espejo de vanidad, amenidades, secadora de cabello, cafetera y kit de cafe, closet, caja de seguridad, plancha y mesa de planchado, escritorio de trabajo, conexion wi-fi y alambrica a internet, llave electronica y telefono con linea directa. PREPAGO: SE LE COBRARÁ UN PREPAGO DEL PRECIO TOTAL EN CUALQUIER MOMENTO, CANCELACIÓN: SE LE COBRARÁ EL PRECIO TOTAL SI CANCELA SU RESERVA O SI ES NO SHOW</aside></aside> 
+				
+				
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-04.png', __FILE__ ); ?>" alt="icono">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-03.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-01.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-05.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<img class="iconoshabitacion hidden-xs hidden-sm" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_icon-02.png', __FILE__ ); ?>" alt="icono" width="60" height="50">
+				<hr class="linealetras" style="border-color:#C9B891;" style="border:2px;" />
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_like.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+				<input type="image" src="<?php echo plugins_url( $iconos_dir . DIRECTORY_SEPARATOR . 'gh_c.png', __FILE__ ); ?>" alt="Submit" width="30" height="30">
+			</article>
+		
+			<article class="col-md-3">
+				<br>
+				<input type="checkbox"> Mejor tarifa garantizada <p>$MXN 2,733.03</p>
+				<p>Con mas beneficios</p>
+				<hr class="linea"/>
+				<h3 align="center">$MXN 2,733.03   /noche</h3>
+				<button class="buttonreserv">Reservar Ahora  </button>
+			</article>
+		</section>
 		
 		<hr class="lineaabajo" />	
 	</div>
