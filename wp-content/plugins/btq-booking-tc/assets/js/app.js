773172d672072d224bd00d9d0d22d0760ddf89ea
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-05-24 13:43:22 -0500

    prueba

--- wp-content/plugins/btq-booking-tc/assets/js/app.js
+++ wp-content/plugins/btq-booking-tc/assets/js/app.js
@@ -47,4 +47,87 @@
 				  
 				});
 
+
+
+				   jQuery(document).ready(function(){ 
+  
+				    jQuery('#vermas1').toggle( 
+				  
+				        // Primer click
+				        function(h){ 
+				            jQuery('#mostrar1').slideDown();
+				            jQuery(this).text('');
+				            h.preventDefault();
+				            
+				            
+				        }, 
+				      
+				        // Segundo click
+				        function(h){ 
+				            jQuery('#mostrar1').slideUp();
+				            jQuery(this).text('Ver mas');
+				            h.preventDefault();
+
+				            
+				        }
+				  
+				    );
+				  
+				});
+
+
+
+				   jQuery(document).ready(function(){ 
+  
+				    jQuery('#vermas2').toggle( 
+				  
+				        // Primer click
+				        function(z){ 
+				            jQuery('#mostrar2').slideDown();
+				            jQuery(this).text('');
+				            z.preventDefault();
+				            
+				            
+				        }, 
+				      
+				        // Segundo click
+				        function(z){ 
+				            jQuery('#mostrar2').slideUp();
+				            jQuery(this).text('Ver mas');
+				            z.preventDefault();
+
+				            
+				        }
+				  
+				    );
+				  
+				});
+
+
+
+				   jQuery(document).ready(function(){ 
+  
+				    jQuery('#vermas').toggle( 
+				  
+				        // Primer click
+				        function(x){ 
+				            jQuery('#mostrar3').slideDown();
+				            jQuery(this).text('');
+				            x.preventDefault();
+				            
+				            
+				        }, 
+				      
+				        // Segundo click
+				        function(x){ 
+				            jQuery('#mostrar3').slideUp();
+				            jQuery(this).text('Ver mas');
+				            x.preventDefault();
+
+				            
+				        }
+				  
+				    );
+				  
+				});
 					
