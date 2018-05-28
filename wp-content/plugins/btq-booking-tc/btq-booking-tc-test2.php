2db7ec31c248194af094c2ce19c479a1acca2445
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-05-28 11:01:33 -0500

    fix

--- wp-content/plugins/btq-booking-tc/btq-booking-tc-test2.php
+++ wp-content/plugins/btq-booking-tc/btq-booking-tc-test2.php
@@ -780,7 +780,7 @@ jQuery(document).ready(function(jQuery){jQuery.datepicker.setDefaults({"closeTex
 				<h3 class="titulo">Habitacion de Lujo King No Reembolsable</h3>
 
 				<aside>La habitación de Lujo King No Reembolsable cuenta con una cama King size y vista a la calle 16 de septiembre. Televisor LCD de 50 pulgadas con control remoto y canales de cable, aire acondicionado, 
-				<a class="vermas">Ver más</a>
+                <p><a class="vermas">Ver más</a></p>
 
 				<aside class="recorrido" style="display:none">mini bar, sala de bano privada con regadera y bañera, espejo de vanidad, amenidades, secadora de cabello, cafetera y kit de cafe, closet, caja de seguridad, plancha y mesa de planchado, escritorio de trabajo, conexion wi-fi y alambrica a internet, llave electronica y telefono con linea directa. PREPAGO: SE LE COBRARÁ UN PREPAGO DEL PRECIO TOTAL EN CUALQUIER MOMENTO, CANCELACIÓN: SE LE COBRARÁ EL PRECIO TOTAL SI CANCELA SU RESERVA O SI ES NO SHOW</aside></aside> 
 				
