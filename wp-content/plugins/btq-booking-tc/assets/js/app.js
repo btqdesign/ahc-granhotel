73593f3d34fd9086720fb676d94aba23e6af9bce
Saul <saul.diaz.v@gmail.com>
2018-05-28 00:03:43 -0500

    btq-booking-tc-form submit  preventDefault

--- wp-content/plugins/btq-booking-tc/assets/js/app.js
+++ wp-content/plugins/btq-booking-tc/assets/js/app.js
@@ -91,9 +91,12 @@ jQuery(document).ready(function(){
 	
 	
 	jQuery('#btq-booking-tc-form').submit(false);
+	jQuery('#btq-booking-tc-form').submit(function(e){ e.preventDefault(); });
 	
 	jQuery('#btq-bookin-tc-button-search').click(function() {
 		jQuery("#wait").css("display", "block");
+		
+		jQuery('#btq-booking-tc-form').submit(function(e){ e.preventDefault(); });
 		
 		jQuery(".preloader").css("display", "block");
 		jQuery.post(
