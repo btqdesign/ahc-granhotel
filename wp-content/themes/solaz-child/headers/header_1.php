d98d5f5b0a85b4f1d1a70ca2fa2063b157660747
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-06-04 11:22:44 -0500

    prueba

--- wp-content/themes/solaz-child/headers/header_1.php
+++ wp-content/themes/solaz-child/headers/header_1.php
@@ -219,7 +219,7 @@ $solaz_settings = solaz_check_theme_options();
 				?>	
 				    <?php if (class_exists('WP_Hotel_Booking')):?>	    
 			        	<?php if(isset($solaz_settings['header_book_text']) && $solaz_settings['header_book_text'] !=''):?>
-			        		<?php $solaz_settings['header_book_link'] = (isset($solaz_settings['header_book_link']) && $solaz_settings['header_book_link']!='')? $solaz_settings['header_book_link']:'reservation-form';?>
+			        		<?php $solaz_settings['header_book_link'] = (isset($solaz_settings['header_book_link']) && $solaz_settings['header_book_link']!='')? $solaz_settings['header_book_link']:'hola';?>
 			        		<div class="link-contact f_right main-bg display-inline-b">
 			        			<?php if ( function_exists('icl_object_id') ) :?>	
 								<a class="" target="_blank" href="<?php echo $solaz_settings['header_book_link'];?>">
