76d0fe92218d9c5d6280891e5fc491a5e5a8e5f8
Saul <saul.diaz.v@gmail.com>
2018-06-07 13:32:44 -0500

    FirebaseUI

--- wp-content/plugins/btq-login/btq-login.php
+++ wp-content/plugins/btq-login/btq-login.php
@@ -28,20 +28,22 @@ defined('ABSPATH') or die('No script kiddies please!');
  * @return string Tag de Firebase
  */
 function btq_login_wp_head(){
-	?>
-		<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
-		<script>
-		  // Initialize Firebase
-		  var config = {
-		    apiKey: "AIzaSyAJKAc_-VwG7Lt_LeSjbNnr8LEzms1WJxk",
-		    authDomain: "btq-ahm-gran-hotel.firebaseapp.com",
-		    databaseURL: "https://btq-ahm-gran-hotel.firebaseio.com",
-		    projectId: "btq-ahm-gran-hotel",
-		    storageBucket: "",
-		    messagingSenderId: "241886061865"
-		  };
-		  firebase.initializeApp(config);
-		</script>
-	<?php
+?>
+	<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+	<script>
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyAJKAc_-VwG7Lt_LeSjbNnr8LEzms1WJxk",
+	    authDomain: "btq-ahm-gran-hotel.firebaseapp.com",
+	    databaseURL: "https://btq-ahm-gran-hotel.firebaseio.com",
+	    projectId: "btq-ahm-gran-hotel",
+	    storageBucket: "",
+	    messagingSenderId: "241886061865"
+	  };
+	  firebase.initializeApp(config);
+	</script>
+	<script src="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.js"></script>
+	<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.css" />
+<?php
 }
 add_action('wp_enqueue_scripts', 'btq_login_wp_head', 1);
