de52da9866e1a61fb4f531bf27afc7e1eb53ac56
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-06-15 11:24:47 -0500

    prueba lenguaje

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,2 +1,22 @@
+
 <?php
-// Silence is golden.
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+switch ($lang){
+    case "fr":
+        //echo "PAGE FR";
+        include("index_fr.php");//include check session FR
+        break;
+    case "it":
+        //echo "PAGE IT";
+        include("index_it.php");
+        break;
+    case "en":
+        //echo "PAGE EN";
+        include("index_en.php");
+        break;        
+    default:
+        //echo "PAGE EN - Setting Default";
+        include("index_en.php");//include EN in all other cases of different lang detection
+        break;
+}
+?>
