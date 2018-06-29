ac6e99316337d4e7f16264b493bf994901a1b4c2
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-06-29 14:03:31 -0500

    revert

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -24,8 +24,8 @@ if (user) {
 
     var email_id = user.email;
     var name = user.displayName;
-    var photo = user.photo;
-    document.getElementById("user_data").innerHTML = "Bienvenido usuario : " + email_id + " " +  name + " " + photo;
+    var photo = user.photoUrl;
+    document.getElementById("user_para").innerHTML = "Bienvenido usuario : " + email_id + " " +  name + " " + photo;
   }
 
 } else {
