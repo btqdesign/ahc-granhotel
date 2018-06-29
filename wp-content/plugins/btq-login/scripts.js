773a5af076a3a085f090533373de7299977feb25
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-06-29 13:44:18 -0500

    test

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -27,6 +27,7 @@ if (user) {
     var photoUrl = user.photoURL;
     document.getElementById("user_para").innerHTML = "Bienvenido usuario : " + email_id + " " +  name + " "
       + '<img src"' + decodeURIComponent(photoUrl) + '">';
+      document.getElementById("user_para").src = "photoUrl";
   }
 
 } else {
