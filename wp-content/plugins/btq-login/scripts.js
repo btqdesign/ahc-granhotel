8bc65b9a383b4a8fcd45a57077d86fc743b7447d
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-03 11:39:57 -0500

    test

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -37,12 +37,10 @@ if (user) {
 
 //Aqui inicia la funcion de registrar un nuevo usuario con email y pass
 function nuevo_usuario(){
-  document.getElementById("recuperar").style.display = "none";
-  document.getElementById("login_div").style.display = "none";
-  document.getElementById("user_div").style.display = "none";
+
   document.getElementById("registro").style.display = "none";  
-  document.getElementById("recuperado").style.display = "none";
   document.getElementById("registro_completado").style.display = "block";
+
       var newuserEmail = document.getElementById("new_email_field").value;
       var newuserPass = document.getElementById("new_password_field").value;
 
@@ -66,7 +64,6 @@ function login(){
       // Errores en caso de que no pueda iniciar sesion
       var errorCode = error.code;
       var errorMessage = error.message;
-      document.getElementById("botones_primarios").style.display = "none";
     });
 }
   //aqui termina login con correo y contraseña
