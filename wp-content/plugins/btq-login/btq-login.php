8f2ca8c50b36138fbd8844a06555aa764860a31c
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-03 12:17:31 -0500

    plugin

--- wp-content/plugins/btq-login/btq-login.php
+++ wp-content/plugins/btq-login/btq-login.php
@@ -47,8 +47,8 @@ function btq_login_wp_head(){
           };
           firebase.initializeApp(config);
         </script>
-      <link rel="stylesheet" type="text/css" href="estilos.css">
-      <script src="scripts.js"></script>
+      <link rel="stylesheet" type="text/css" href="<?php echo plugins_url( 'estilos.css', __FILE__ ); ?>">
+      <script src="<?php echo plugins_url( 'scripts.js', __FILE__ ); ?>"></script>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
