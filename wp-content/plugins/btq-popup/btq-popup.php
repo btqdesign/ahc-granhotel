84761caacea21724cbabba2b951695f4a6cdca42
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-12 15:10:58 -0500

    test

--- wp-content/plugins/btq-popup/btq-popup.php
+++ wp-content/plugins/btq-popup/btq-popup.php
@@ -33,14 +33,18 @@ load_plugin_textdomain('btq-login', false, basename( dirname( __FILE__ ) ) . '/l
  * @author Saúl Díaz
  * @return void Integra CSS y JS al frond-end del sitio.
  */
-function btq_popup_scripts() {
+function btq_login_scripts() {
     if (!is_admin()) {
 	    wp_enqueue_style( 'bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css', 'solaz-child-style','4.1.1');
+	    wp_enqueue_style( 'btq-login', plugins_url( 'estilos.css', __FILE__ ), array('solaz-child-style','bootstrap4'),'1.0');
+	    wp_enqueue_script( 'firebase', 'https://www.gstatic.com/firebasejs/5.0.4/firebase.js', array(), '5.0.4');
+	    //wp_enqueue_script( 'firebase-auth', 'https://www.gstatic.com/firebasejs/5.0.4/firebase-auth.js', array('firebase'), '5.0.4');
+	    wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(), '1.14.3');
 	    wp_enqueue_script( 'bootstrap4js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array(), '4.1.1');
-	    wp_enqueue_script( 'btq-login-js', plugins_url( 'script.js', __FILE__ ), array('firebase'), '1.0');
+	    wp_enqueue_script( 'btq-login-js', plugins_url( 'scripts.js', __FILE__ ), array('firebase'), '1.0');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'btq_popup_scripts', 1 );
+add_action( 'wp_enqueue_scripts', 'btq_login_scripts', 1 );
 
 /**
  * Declara el Widget de BTQ Login en VisualCompouser.
@@ -48,16 +52,16 @@ add_action( 'wp_enqueue_scripts', 'btq_popup_scripts', 1 );
  * @author Saúl Díaz
  * @return void Widget de BTQ Login en VisualCompouser.
  */
-function btq_popup_VC() {
+function btq_booking_login_VC() {
 	vc_map(array(
-		'name'     => __( 'BTQ popup', 'btq-popup' ),
-		'base'     => 'btq-popup',
+		'name'     => __( 'BTQ Login', 'btq-login' ),
+		'base'     => 'btq-login',
 		'class'    => '',
-		'category' => __( 'Content', 'btq-popup'),
+		'category' => __( 'Content', 'btq-login'),
 		'icon'     => plugins_url( 'assets/images/iconos' . DIRECTORY_SEPARATOR . 'btqdesign-logo.png', __FILE__ )
 	));
 }
-add_action( 'vc_before_init', 'btq_popup_VC' );
+add_action( 'vc_before_init', 'btq_booking_login_VC' );
 
 /**
  * Función del shortcode que imprime el BTQ Login en el frond-end.
@@ -65,11 +69,10 @@ add_action( 'vc_before_init', 'btq_popup_VC' );
  * @author Saúl Díaz
  * @return string Imprime el BTQ Booking TC
  */
-function btq_popup_shortcode() {
+function btq_login_shortcode() {
 	ob_start();
 	?>
-    
-    <body>  
+
       <!-- Modal -->
       <div class="modal fade" id="Top5razones" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -84,12 +87,35 @@ function btq_popup_shortcode() {
         </div>
       </div>
 
-  </body>
 
 	<?php
 	$out = ob_get_clean();
 	
 	return $out;
 } // function btq_login_shortcode()
-add_shortcode( 'btq-popup', 'btq_popup_shortcode' );
-add_action('wp_footer', 'btq_popup');
+add_shortcode( 'btq-login', 'btq_login_shortcode' );
+
+
+
+function btq_login_modals() {
+	?>
+
+      <!-- Modal -->
+      <div class="modal fade" id="Top5razones" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div style="padding: 0px;" class="modal-body">
+              <img src="imagenes/pop_up_img.jpg" alt="Top 5 razones por las que conviene reservar.">
+            </div>
+            <div style="padding: 0px;" class="modal-footer">
+                <a href="https://granhoteldelaciudaddemexico.com.mx/en/learn-about-the-benefits-of-booking-with-us/"><img src="imagenes/btn_pop_up.jpg" alt="Top 5 razones por las que conviene reservar."></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+	<?php
+}
+add_action('wp_footer', 'btq_login_modals');
