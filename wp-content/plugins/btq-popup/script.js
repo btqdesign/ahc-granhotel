4eb2d1920ee4d7dde641247e00c7ee9f16d175b7
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-13 11:04:56 -0500

    delay

--- wp-content/plugins/btq-popup/script.js
+++ wp-content/plugins/btq-popup/script.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function(){
-    jQuery('#Top5razones').modal('show').addClass('load').wait(9000).addClass('done');
+    jQuery('#Top5razones').modal('show').delay( 8000 )
 });
 
