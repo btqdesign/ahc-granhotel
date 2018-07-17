a1edec73d4cc5d79a0eb7e49641c95861a26ca6d
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-17 11:12:32 -0500

    prueba

---
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Plugin Name: BTQ popup
+ * Plugin URI: https://hotel.idevol.net/wp-content/plugins/btq-popup/btq-popup.html
+ * Description: Popup autodesplegable.
+ * Version: 1.0
+ * Author: BTQ Design
+ * Author URI: http://btqdesign.com/
+ * Requires at least: 4.9.6
+ * Tested up to: 4.9.6
+ * 
+ * Text Domain: btq-login
+ * Domain Path: /languages
+ * 
+ * @package btq-login
+ * @category Core
+ * @author BTQ Design
+ */
+
+
+// Exit if accessed directly
+defined('ABSPATH') or die('No script kiddies please!');
+
+/** 
+ * Establece el dominio correcto para la carga de traducciones
+ */
+load_plugin_textdomain('btq-popup', false, basename( dirname( __FILE__ ) ) . '/languages');
+
+/**
+ * Añade a WordPress los assets JS y CSS necesarios para el Grid.
+ *
+ * @author José Antonio del Carmen
+ * @return void Integra CSS y JS al frond-end del sitio.
+ */
+function btq_popup_scripts() {
+    if (!is_admin()) {
+	    wp_enqueue_style( 'bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css', 'solaz-child-style','4.1.1');
+	    wp_enqueue_script( 'firebase', 'https://www.gstatic.com/firebasejs/5.0.4/firebase.js', array(), '5.0.4');
+	    wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(), '1.14.3');
+	    wp_enqueue_script( 'bootstrap4js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array(), '4.1.1');
+	    wp_enqueue_script( 'btq-popup-js', plugins_url( 'script.js', __FILE__ ), array('firebase'), '1.0');
+	}
+}
+add_action( 'wp_enqueue_scripts', 'btq_popup_scripts', 1 );
+
+/**
+ * Declara el Widget de BTQ Login en VisualCompouser.
+ *
+ * @author José Antonio del Carmen
+ * @return void Widget de BTQ popup en VisualCompouser.
+ */
+function btq_emfoco_VC() {
+	vc_map(array(
+    'name'     => __( 'BTQ emfoco', 'btq-emfoco' ),
+		'base'     => 'btq-emfoco',
+		'class'    => '',
+		'category' => __( 'Content', 'btq-emfoco'),
+		'icon'     => plugins_url( 'assets/images/iconos' . DIRECTORY_SEPARATOR . 'btqdesign-logo.png', __FILE__ )
+	));
+}
+add_action( 'vc_before_init', 'btq_emfoco_VC' );
+
+
+
+function btq_emfoco() {
+
+	?>
+      <?php
+class MySettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Settings Admin', 
+            'My Settings', 
+            'manage_options', 
+            'my-setting-admin', 
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'my_option_name' );
+        ?>
+        <div class="wrap">
+            <h1>My Settings</h1>
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'my_option_group' );
+                do_settings_sections( 'my-setting-admin' );
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {        
+        register_setting(
+            'my_option_group', // Option group
+            'my_option_name', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'My Custom Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'my-setting-admin' // Page
+        );  
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title 
+            array( $this, 'id_number_callback' ), // Callback
+            'my-setting-admin', // Page
+            'setting_section_id' // Section           
+        );      
+
+        add_settings_field(
+            'title', 
+            'Title', 
+            array( $this, 'title_callback' ), 
+            'my-setting-admin', 
+            'setting_section_id'
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function title_callback()
+    {
+        printf(
+            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
+            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+        );
+    }
+}
+
+if( is_admin() )
+    $my_settings_page = new MySettingsPage();
+    
+	
+}
+
+add_action('wp_footer', 'btq_emfoco');
