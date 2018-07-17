d58dfd34e5a1f397adc31a03c1b74542a3a3284a
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-17 11:24:53 -0500

    script

--- wp-content/plugins/plugin emfoco/emfoco.php
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -18,26 +18,22 @@
  */
 
 
-// Exit if accessed directly
-defined('ABSPATH') or die('No script kiddies please!');
-
-
 /**
  * Añade a WordPress los assets JS y CSS necesarios para el Grid.
  *
  * @author José Antonio del Carmen
  * @return void Integra CSS y JS al frond-end del sitio.
  */
-function btq_popup_scripts() {
+function btq_emfoco_scripts() {
     if (!is_admin()) {
 	    wp_enqueue_style( 'bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css', 'solaz-child-style','4.1.1');
 	    wp_enqueue_script( 'firebase', 'https://www.gstatic.com/firebasejs/5.0.4/firebase.js', array(), '5.0.4');
 	    wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(), '1.14.3');
 	    wp_enqueue_script( 'bootstrap4js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array(), '4.1.1');
-	    wp_enqueue_script( 'btq-popup-js', plugins_url( 'script.js', __FILE__ ), array('firebase'), '1.0');
+	    wp_enqueue_script( 'btq-emfoco-js', plugins_url( 'script.js', __FILE__ ), array('firebase'), '1.0');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'btq_popup_scripts', 1 );
+add_action( 'wp_enqueue_scripts', 'btq_emfoco_scripts', 1 );
 
 /**
  * Declara el Widget de BTQ Login en VisualCompouser.
