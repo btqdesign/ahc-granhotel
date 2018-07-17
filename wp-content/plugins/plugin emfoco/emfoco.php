48f1a9e013bd84f17dcd0662ee45dcef100c41ae
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-17 12:18:02 -0500

    opciones

--- wp-content/plugins/plugin emfoco/emfoco.php
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -95,7 +95,7 @@ add_action( 'vc_before_init', 'btq_emfoco_VC' );
                 
                   <tr>
                       <th>Descripción del sector:</th>
-                      <td><textarea placeholder="Descripcion" name="map_option_2" rows="10" cols="50"><?php echo esc_attr( get_option('map_option_2') ); ?></textarea></td>
+                      <td><textarea placeholder="Descripcion" name="map_option_3" rows="10" cols="50"><?php echo esc_attr( get_option('map_option_3') ); ?></textarea></td>
                   </tr>
        
                   
