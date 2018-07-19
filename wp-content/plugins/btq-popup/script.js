9b72989cf48b14698455db101af9503fb84c86d0
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-19 09:58:36 -0500

    test

--- wp-content/plugins/btq-popup/script.js
+++ wp-content/plugins/btq-popup/script.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function(){
-	setTimeout(function(){
-		if(!Cookies.get('modalShown')) {
-			jQuery('#Top5razones').modal('show');
-			Cookies.set('modalShown', true);
-		}
-	},9000);
+	jQuery('#Top5razones').modal('show');
 });
