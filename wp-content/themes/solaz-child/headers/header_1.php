94fe16651021c95b2d461bd112a280719f28b9dc
Saul <saul.diaz.v@gmail.com>
2018-07-18 20:49:54 -0500

    Ident code

--- wp-content/themes/solaz-child/headers/header_1.php
+++ wp-content/themes/solaz-child/headers/header_1.php
@@ -233,20 +233,20 @@ $solaz_settings = solaz_check_theme_options();
 					</ul>
 				</div>
 				*/ ?>
-					
-					<?php if (class_exists('WP_Hotel_Booking')):?>	    
-			        	<?php if(isset($solaz_settings['header_book_text']) && $solaz_settings['header_book_text'] !=''):?>
-			        		<?php $solaz_settings['header_book_link'] = (isset($solaz_settings['header_book_link']) && $solaz_settings['header_book_link']!='')? $solaz_settings['header_book_link']:'hola';?>
-							<div class="link-contact f_right main-bg display-inline-b">
-			        			<?php if ($language == 'es'):?>	
+				
+				<?php if (class_exists('WP_Hotel_Booking')):?>
+					<?php if(isset($solaz_settings['header_book_text']) && $solaz_settings['header_book_text'] !=''):?>
+						<?php $solaz_settings['header_book_link'] = (isset($solaz_settings['header_book_link']) && $solaz_settings['header_book_link']!='')? $solaz_settings['header_book_link']:'hola';?>
+						<div class="link-contact f_right main-bg display-inline-b">
+							<?php if ($language == 'es'):?>
 								<a class="" target="_blank" href="https://reservations.travelclick.com/131328?themeid=13670#/guestsandrooms">Reservar Ahora</a>
-								<?php else:?>
-								<a class="" target="_blank" href="https://reservations.travelclick.com/95698?themeid=13671#/guestsandrooms">Book Now</a>			
-								<?php endif;?>
-			        		</div>
-			        	<?php endif;?>	
-					<?php endif; ?>	        		        	
-	        </div>      	
-        </div>  
+							<?php else:?>
+								<a class="" target="_blank" href="https://reservations.travelclick.com/95698?themeid=13671#/guestsandrooms">Book Now</a>
+							<?php endif;?>
+						</div>
+					<?php endif;?>
+				<?php endif; ?>
+	        </div>
+        </div>
 </div>
 <!-- Menu -->
