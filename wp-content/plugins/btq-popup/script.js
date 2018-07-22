2a0656bb1fdd0d04f7ad44825977d461fb973832
Saul <saul.diaz.v@gmail.com>
2018-07-21 20:17:26 -0500

    Debug

--- wp-content/plugins/btq-popup/script.js
+++ wp-content/plugins/btq-popup/script.js
@@ -1,8 +1,8 @@
-//jQuery(document).ready(function(){
+jQuery(document).ready(function(){
 	//setTimeout(function(){
 		//if(!Cookies.get('modalShown')) {
 			jQuery('#Top5razones').modal('show');
 			//Cookies.set('modalShown', true);
 		//}
 	//},9000);
-//});
+});
