33a5304ff5aa9845cbc6d37abbd448af1853dc6a
Saul <saul.diaz.v@gmail.com>
2018-07-22 19:15:05 -0500

    Ordern

--- wp-content/plugins/btq-booking/btq-booking.php
+++ wp-content/plugins/btq-booking/btq-booking.php
@@ -539,7 +539,11 @@ function btq_booking_admin_packages($hotelCode) {
 	}
 	?>
 	<table cellpadding="3" cellspacing="2" border="1" style="margin-top: 10px; border-color: #333;">
-		<tr align="center" style="background-color: #333; color: white;"><th><?php _e('Rate Plan Code','btq-booking'); ?></th><th><?php _e('Rate Plan Name','btq-booking'); ?></th><th><?php _e('Folder With Pictures','btq-booking'); ?></th></tr>
+		<tr align="center" style="background-color: #333; color: white;">
+			<th><?php _e('Rate Plan Code','btq-booking'); ?></th>
+			<th><?php _e('Rate Plan Name','btq-booking'); ?></th>
+			<th><?php _e('Folder With Pictures','btq-booking'); ?></th>
+		</tr>
 	<?php
 	foreach($arrayRatePlan as $elementRatePlan){			
 		$RatePlanCode = $elementRatePlan['!RatePlanCode'];
@@ -551,7 +555,11 @@ function btq_booking_admin_packages($hotelCode) {
 		$images_dir = plugin_dir_path( __FILE__ ) . $images_packages_path . $RatePlanCode;
 		$folder_with_pictures = (is_dir($images_dir)) ? __('Yes','btq-booking') : __('No','btq-booking');
 		?>
-		<tr><td style="background-color: #EEE;"><?php echo $RatePlanCode; ?></td><td style="background-color: #EEE;"><?php echo htmlentities($elementRatePlan['!RatePlanName']); ?></td><td align="center" style="background-color: #EEE;"><?php echo $folder_with_pictures; ?></td></tr>
+		<tr>
+			<td style="background-color: #EEE;"><?php echo $RatePlanCode; ?></td>
+			<td style="background-color: #EEE;"><?php echo htmlentities($elementRatePlan['!RatePlanName']); ?></td>
+			<td align="center" style="background-color: #EEE;"><?php echo $folder_with_pictures; ?></td>
+		</tr>
 		<?php
 	}
 	?>
@@ -599,7 +607,11 @@ function btq_booking_admin_rooms($hotelCode) {
 	
 	?>
 	<table cellpadding="3" cellspacing="2" border="1" style="margin-top: 10px; border-color: #333;">
-		<tr align="center" style="background-color: #333; color: white;"><th><?php _e('Room Type Code','btq-booking'); ?></th><th><?php _e('Room Type Name','btq-booking'); ?></th><th><?php _e('Folder With Pictures','btq-booking'); ?></th></tr>
+		<tr align="center" style="background-color: #333; color: white;">
+			<th><?php _e('Room Type Code','btq-booking'); ?></th>
+			<th><?php _e('Room Type Name','btq-booking'); ?></th>
+			<th><?php _e('Folder With Pictures','btq-booking'); ?></th>
+		</tr>
 	<?php
 	foreach($RoomType as $elementRoomType){
 		$RoomAmenities[] = $elementRoomType['Amenities']['Amenity'];
@@ -609,7 +621,11 @@ function btq_booking_admin_rooms($hotelCode) {
 		$folder_with_pictures = (is_dir($images_dir)) ? __('Yes','btq-booking') : __('No','btq-booking');
 		
 		?>
-		<tr><td style="background-color: #EEE;"><?php echo $elementRoomType['!RoomTypeCode']; ?></td><td style="background-color: #EEE;"><?php echo htmlentities($elementRoomType['!RoomTypeName']); ?></td><td align="center" style="background-color: #EEE;"><?php echo $folder_with_pictures; ?></td></tr>
+		<tr>
+			<td style="background-color: #EEE;"><?php echo $elementRoomType['!RoomTypeCode']; ?></td>
+			<td style="background-color: #EEE;"><?php echo htmlentities($elementRoomType['!RoomTypeName']); ?></td>
+			<td align="center" style="background-color: #EEE;"><?php echo $folder_with_pictures; ?></td>
+		</tr>
 		<?php
 	}
 	?>
