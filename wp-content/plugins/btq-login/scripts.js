f49cdde13866ff5e593d260e423c5375a491f5a6
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-26 10:50:56 -0500

    login fb anterior

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -139,23 +139,25 @@ function facebook_login(){
 
   var provider = new firebase.auth.FacebookAuthProvider();
   
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+    // Te da el token de inicio de sesion de facebook
+    var token = result.credential.accessToken;
+    // Obtiene la informacion del usuario
     var user = result.user;
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("botones_primarios").style.display = "none";
+    document.getElementById("botones_primarios_modals").style.display = "none";
+        // En caso de no iniciar sesion correctamente se ejecuta la siguiente funcion
   }).catch(function(error) {
-    // Handle Errors here.
+    // Errores en caso de no iniciar sesion
     var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
+    // El correo ya esta registrado en la base de datos
     var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
+    // La credencial Auth ya esta usada
     var credential = error.credential;
     // ...
+    window.alert=error.message;
   });
 }
 //Aqui termina funcion para iniciar sesion con facebook
