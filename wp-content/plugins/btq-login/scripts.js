8dd0edcac22fb7e55947aa42b8ffc8cbb2086a35
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-08-03 14:52:29 -0500

    test

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -37,39 +37,25 @@ if (user) {
 
 //Aqui inicia la funcion de registrar un nuevo usuario con email y pass
 function nuevo_usuario(){
+      document.getElementById("registro").style.display = "none";
+      document.getElementById("registro_completado").style.display = "block";   
+
       var newuserEmail = document.getElementById("new_email_field").value;
       var newuserPass = document.getElementById("new_password_field").value;
       
-      /*
-      firebase.auth().createUserWithEmailAndPassword(newuserEmail, newuserPass).catch(function(error) {
-      // Errores en caso de que no se pueda registrar
-      var errorCode = error.code;
-      var errorMessage = error.message; 
-      window.alert("Error:" + error.message);
-      });*/
-      
-    firebase.auth().createUserWithEmailAndPassword(newuserEmail, newuserPass).then(function(user) {
-        //var user = firebase.auth().currentUser;
-        //logUser(user); // Optional
-
-      document.getElementById("registro").style.display = "none";
-      document.getElementById("registro_completado").style.display = "block";   
-      
-      var n = 10;
-      var l = document.getElementById("contador");
-      window.setInterval(function(){
-        if ( n > -1){
-        l.innerHTML = "Esta ventana se cerrara automaticamente en: " + n;
-        n--;
-          }
-      },1000);
-      setTimeout( function(){
-        jQuery('#Registro').modal('hide');
-        document.getElementById("user_div").style.display = "block";
+      firebase.auth().createUserWithEmailAndPassword(newuserEmail, newuserPass).then(function(user) {
+        var n = 10;
+        var l = document.getElementById("contador");
+        window.setInterval(function(){
+          if ( n > -1){
+          l.innerHTML = "Esta ventana se cerrara automaticamente en: " + n;
+          n--;
+            }
+        },1000);
+       setTimeout( function(){
+          jQuery('#Registro').modal('hide');
+          document.getElementById("user_div").style.display = "block";
       },10000);
-      
-
-        
       
     }, function(error) {
         // Handle Errors here.
