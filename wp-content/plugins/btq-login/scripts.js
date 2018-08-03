3d445f7250437fcbca7efe8f7bdb7ddc20450647
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-08-03 13:31:49 -0500

    no modal hide

--- wp-content/plugins/btq-login/scripts.js
+++ wp-content/plugins/btq-login/scripts.js
@@ -57,9 +57,7 @@ function nuevo_usuario(){
         n--;
           }
       },1000);
-    
-      jQuery('#Registro').modal('hide')
-      document.getElementById("user_div").style.display = "block"; 
+          document.getElementById("user_div").style.display = "block"; 
     
 }
 //Aqui termina la funcion de registrar un nuevo usuario con email y pass
