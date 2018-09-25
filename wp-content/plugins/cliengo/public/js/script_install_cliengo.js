94ce55d289df25298ad7654fc7fe218130a9fd80
idevol <saul.diaz.v@gmail.com>
2018-09-25 11:22:27 -0500

    cliengo

--- wp-content/plugins/cliengo/public/js/script_install_cliengo.js
+++ wp-content/plugins/cliengo/public/js/script_install_cliengo.js
@@ -1,1 +1,1 @@
-(function(){var ldk=document.createElement("script"); ldk.type="text/javascript";ldk.async=true; ldk.src="https://s.cliengo.com/weboptimizer/5b351e9ce4b0a56f035701ff/5b351e9ce4b0a56f03570204.js"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ldk, s);})();
+(function(){var ldk=document.createElement("script"); ldk.type="text/javascript";ldk.async=true; ldk.src="https://s.cliengo.com/weboptimizer/5a5fb5cce4b0767314ffe110/5a5fb5d3e4b0767314ffe11c.js"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ldk, s);})();
